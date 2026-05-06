ca25768ee1bc2e961590282cef86e501f86f9f59
nicolels1 <nicolels1@al.insper.edu.br>
2026-05-06 10:54:24 -0300

    inicio

---
+++ jogo.py
@@ -0,0 +1,29 @@
+
+import pygame
+
+pygame.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((1280, 720)) #No final mudar essa parte para tela cheia 
+pygame.display.set_caption('Jogo das Divas Pop')
+
+# ----- Inicia estruturas de dados
+game = True
+
+# ===== Loop principal =====
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+    # ----- Gera saídas
+    window.fill((255, 255, 255))  # Preenche com a cor branca
+
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+

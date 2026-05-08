ba43b61f587d357e5d5fd7e8bbd08f069e6cc357
nicolels1 <nicolels1@al.insper.edu.br>
2026-05-08 09:45:28 -0300

    tela de fundo

--- jogo.py
+++ jogo.py
@@ -7,6 +7,10 @@ pygame.init()
 window = pygame.display.set_mode((1280, 720)) #No final mudar essa parte para tela cheia 
 pygame.display.set_caption('Jogo das Divas Pop')
 
+# ----- Carrega imagem de fundo
+background = pygame.image.load('assests/Background inicial.png').convert()
+background = pygame.transform.scale(background, (1280, 720))
+
 # ----- Inicia estruturas de dados
 game = True
 
@@ -19,11 +23,10 @@ while game:
             game = False
 
     # ----- Gera saídas
-    window.fill((255, 255, 255))  # Preenche com a cor branca
+    window.blit(background, (0, 0))  # Desenha o fundo na tela
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
-

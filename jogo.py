88479abc7d6d01b9a861a05ecb71bceb735efb0e
nicolels1 <nicolels1@al.insper.edu.br>
2026-05-08 15:24:18 -0300

    musicas e sons

--- jogo.py
+++ jogo.py
@@ -2,17 +2,21 @@
 import pygame
 
 pygame.init()
+pygame.mixer.init()
 
 # ----- Gera tela principal
 window = pygame.display.set_mode((1280, 720)) #No final mudar essa parte para tela cheia 
 pygame.display.set_caption('Jogo das Divas Pop')
 
 # ----- Carrega imagem de fundo
-background = pygame.image.load('assests/Background inicial.png').convert()
+background = pygame.image.load('assests/imagens/tela inicio.png').convert()
 background = pygame.transform.scale(background, (1280, 720))
 
+pygame.mixer.music.load('assests/sons/ophelia.ogg') # mudar para a musica de introdução
+pygame.mixer.music.play(-1)  # toca em loop até fechar o jogo
 # ----- Inicia estruturas de dados
 game = True
+
 
 # ===== Loop principal =====
 while game:
@@ -21,6 +25,7 @@ while game:
         # ----- Verifica consequências
         if event.type == pygame.QUIT:
             game = False
+        
 
     # ----- Gera saídas
     window.blit(background, (0, 0))  # Desenha o fundo na tela
